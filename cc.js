68726f7d0fa1d29b98159d839253a2f0d75187c1
༺ ᴹᴿ乂ꘘคнαᴅ࿐ <154923346+fahad757hg@users.noreply.github.com>
2024-02-16 20:46:39 +0500

    html and css

---
+++ cc.js
@@ -0,0 +1,28 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        if (i === index) {
+            slide.style.display = 'block';
+        } else {
+            slide.style.display = 'none';
+        }
+    });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
+
+function prevSlide() {
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    showSlide(currentSlide);
+}
+
+// Initial setup
+showSlide(currentSlide);
+
+// Automatic slideshow
+setInterval(nextSlide, 3000);  // Change slide every 3 seconds
